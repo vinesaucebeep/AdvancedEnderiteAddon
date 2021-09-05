4c5234b59a6a51c39da15447ad265b3ffbc24aa2
vinesaucebeep <88556555+vinesaucebeep@users.noreply.github.com>
2021-09-05 19:34:44 +0200

    Add files via upload

---
+++ src/main/java/com/beep67/advenderite/AdvEnderite.java
@@ -0,0 +1,34 @@
+package com.beep67.advenderite;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.Blocks;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.InterModComms;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
+import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
+import net.minecraftforge.fml.event.server.FMLServerStartingEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.util.stream.Collectors;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod("advenderite")
+public class AdvEnderite {
+
+    public static final String MOD_ID = "advenderite";
+
+    private static final Logger LOGGER = LogManager.getLogger();
+
+    public AdvEnderite() {
+
+        IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
+    }
+}
